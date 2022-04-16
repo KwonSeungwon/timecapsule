e154f67ca404625ec0e57111f18b18e0c3d00d71
seungwon <loveyoo41@gmail.com>
2022-04-16 15:03:57 +0900

    타임캡슐 프로젝트 초기세팅

---
+++ src/main/java/com/mini/timecapsule/TimecapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.mini.timecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimecapsuleApplication.class, args);
+    }
+
+}
