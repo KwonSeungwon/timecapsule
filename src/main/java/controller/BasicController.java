76727b06b968248de38050fb7330f32eab716ea0
seungwon <loveyoo41@gmail.com>
2022-04-19 22:57:28 +0900

    1차푸시

--- src/main/java/controller/BasicController.java
+++ src/main/java/controller/BasicController.java
@@ -1,4 +1,14 @@
 package controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BasicController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "home.html";
+    }
 }
