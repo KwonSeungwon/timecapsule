e5638e5850fe2f69b85e6fd3f36c040288e25850
joranegs92 <joranegs92@gmail.com>
2022-04-19 22:43:42 +0900

    test

--- src/main/java/controller/BasicController.java
+++ src/main/java/controller/BasicController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Slf4j
 public class BasicController {
+    @RequestMapping("/")
+    public String home(){
+        log.info("home Controller");
+        return "home"; //home.jsp 파일로 찾아가서 타임리프파일을 찾아간다 .
+    }
 }
