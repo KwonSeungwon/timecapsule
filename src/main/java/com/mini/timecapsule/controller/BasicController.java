044fd8e7deaa4f5a89861646a71d12ba2be8f9c9
joranegs92 <joranegs92@gmail.com>
2022-04-20 21:55:36 +0900

    있을곳에 있어야 페이지가 돌아간다.

--- src/main/java/com/mini/timecapsule/controller/BasicController.java
+++ src/main/java/com/mini/timecapsule/controller/BasicController.java
@@ -1,11 +1,12 @@
-package controller;
+package com.mini.timecapsule.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @Slf4j
 
-@Controller
+@RestController
 public class BasicController {
     @RequestMapping("/")
     public String home(){
---
+++ src/main/java/com/mini/timecapsule/model/BasicModel.java
@@ -0,0 +1,4 @@
+package com.mini.timecapsule.model;
+
+public class BasicModel {
+}
