7b8e3eb9523940311471a34236796bb99083acb6
joranegs92 <joranegs92@gmail.com>
2022-08-07 21:33:28 +0900

    html완스엉

--- vue/src/components/LoginTest.vue
+++ vue/src/components/LoginTest.vue
@@ -1,78 +1,84 @@
 <template>
-  <html>
-  <head>
-<!--    <link href="https://fonts.googleapis.com/css?family=DungGeunMo&display=swap" rel="stylesheet"/>
-    <link href="https://fonts.googleapis.com/css?family=SF+Pro+Text&display=swap" rel="stylesheet"/>-->
-  </head>
-  <body>
   <div class="field">
   <div class="content">
     <div class="name"></div>
     <span class="title">타임캡슐 확인</span>
     <div style="top: 129px; position: absolute; left: 41px;">
       <div class="box">
-        <a>좌표</a>
+        <a style="margin-left: 48px;">좌표</a>
       </div>
       <div style="margin-top: 8px;">
         <div class="inputField">
-          <input id="id" placeholder="좌표명">
-          <!--      <div class="v229_584"></div>-->
+          <input v-model="id" id="id" placeholder="좌표명">
+
         </div>
       </div>
     </div>
 
-    <span class="v238_590">좌표가 없으신가요? 새로 만들기 ></span>
-    <div style="top: 305px; left: 41px; position: absolute;">
-      <div class="v229_595">
-        <div class="v229_596">
-          <div class="v229_597"></div>
-          <span class="v229_598"></span></div>
-        <div class="v229_599">
-          <div class="v229_600"></div>
-          <input id="id" placeholder="비밀번호"></div>
+    <span class="text">좌표가 없으신가요? 새로 만들기 </span>
+    <div style="top: 270px; left: 41px; position: absolute;">
+
+      <div class="box">
+        <a style="margin-left: 33px;">비밀번호</a>
       </div>
+      <div style="margin-top: 8px;">
+        <div class="inputField">
+          <input id="id" v-model="pw" placeholder="숫자 4자리">
+        </div>
+      </div>
+      <p class="error-content">{{error}}</p>
     </div>
 
-    <div class="v229_631">
-      <div class="next"></div>
-      <div class="cancel"></div>
-      <span class="v229_634">취소</span><span class="v229_635">다음</span></div>
+    <div class="footer">
+      <div class="next" @click="next()">
+        <div style="margin-top: 14px; margin-left: 32px;">
+          <span style=" color: rgba(255, 255, 255, 1); opacity: 1; text-align: center;">다음</span>
+        </div>
+      </div>
+      <div class="cancel">
+        <div style="margin-top: 14px; margin-left: 32px;">
+          <span style=" color: rgba(255, 255, 255, 1); opacity: 1; text-align: center;">취소</span>
+        </div>
+      </div>
+     </div>
     <div class="name"></div>
-    <div class="v229_609">
-      <div class="v229_611">
-        <div class="v229_612">
-          <div class="v229_613"></div>
-          <div class="v229_614"></div>
-          <div class="v229_615"></div>
-        </div>
-        <div class="name"></div>
-        <div class="name"></div>
-      </div>
-    </div>
+
   </div>
   </div>
-  </body>
-  </html>
-  <!--  <div class="field">
-      <div class="title">
-        <p>내 좌표</p>
-      </div>
-      <div class="content">
-        <div class="input-field">
-          <input id="id" placeholder="좌표명">
-        </div>
-        <div class="input-field">
-          <input id="pw" placeholder="password" type="password">
-        </div>
-        <p>좌표가 없으신가요? <router-link to="/join">새로 만들기 ></router-link></p>
-      </div>
-      <Footer></Footer>
-    </div>-->
+
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "cLogin"
+  name: "cLoginTest",
+  data () {
+    return {
+      id : null,
+      pw : null,
+      error : null
+    }
+  },
+  methods : {
+    next: function() {
+      if(this.id ==null){
+        this.error = '아이디가 입력되지 않았습니다.';
+
+      }else if(this.pw == null){
+        this.error = '비밀번호가 입력되지 않았습니다.';
+      }else{
+        axios.get('/api/timecapsule/user',{
+          params :{
+            id : 1,
+            password: 'test'
+          }
+        }).then((res)=> {
+          console.log(res);
+        });
+      }
+    }
+  }
 }
 </script>
 
@@ -93,7 +99,7 @@ body {
 }
 .content {
   width: 375px;
-  height: 812px;
+  height: 700px;
   opacity: 1;
   position: absolute;
   overflow: hidden;
@@ -103,19 +109,6 @@ body {
   color: #fff;
 }
 
-/*.v229_577 {
-  width: 341px;
-  height: 653px;
-  !*  background: url("../images/v229_577.png");*!
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 0.5;
-  position: absolute;
-  top: 79px;
-  left: 17px;
-  overflow: hidden;
-}*/
 
 .title {
   width: 166px;
@@ -144,39 +137,10 @@ body {
 .box a{
   color: white;
   text-shadow: -1px 0 #677ce3, 0 1px #3949b7, 1px 0 #000, 0 -1px #000;
-  font-size: 13px;
   margin-left: 48px;
   position: absolute;
   top: 11px;
-}
-.v254_1477 {
-  width: 124px;
-  height: 37px;
-  /*  background: url("../images/v254_1477.png");*/
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-}
-
-.v254_1479 {
-  width: 49px;
-  height: 12px;
-  background: rgba(255, 255, 255, 1);
-  opacity: 1;
-  position: absolute;
-  top: 12px;
-  left: 36px;
-}
-
-.v254_1478 {
-  width: 52px;
-  height: 14px;
-  background: rgba(37, 76, 175, 1);
-  opacity: 1;
-  position: absolute;
-  top: 10px;
-  left: 34px;
+  font-size: 13px;
 }
 
 .inputField input{
@@ -197,38 +161,11 @@ body {
   border-radius: 8px;
 }
 
-.v229_584 {
-  width: 293px;
-  height: 42px;
-  /*background: url("../images/v229_584.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-
-.v229_585 {
-  width: 177px;
-  color: rgba(51, 51, 51, 1);
-  position: absolute;
-  top: 13px;
-  left: 13px;
-  font-family: DungGeunMo;
-  font-weight: Regular;
-  font-size: 15px;
-  opacity: 1;
-  text-align: left;
-}
-
-.v238_590 {
+.text {
   width: 179px;
   color: rgba(37, 76, 175, 1);
   position: absolute;
-  top: 271px;
+  top: 225px;
   left: 44px;
   font-family: DungGeunMo;
   font-weight: Regular;
@@ -237,113 +174,17 @@ body {
   text-align: left;
 }
 
-.v229_595 {
-  width: 293px;
-  height: 87px;
-  /* background: url("../assets/images/myCodinate.png");;*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 305px;
-  left: 41px;
-  overflow: hidden;
-}
 
-.v229_596 {
-  width: 124px;
-  height: 37px;
-    background: url("../assets/images/password.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-
-.v229_597 {
-  width: 124px;
-  height: 37px;
-  /*  background: url("../images/v229_597.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-
-.v229_598 {
-  width: 60px;
-  color: rgba(255, 255, 255, 1);
-  position: absolute;
-  top: 10px;
-  left: 31px;
-  font-family: DungGeunMo;
-  font-weight: Regular;
-  font-size: 15px;
-  opacity: 1;
-  text-align: center;
-}
-
-.v229_599 {
-  width: 293px;
-  height: 42px;
-  /*  background: url("../images/v229_599.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 45px;
-  left: 0px;
-  overflow: hidden;
-}
-
-.v229_600 {
-  width: 293px;
-  height: 42px;
-  /*  background: url("../images/v229_600.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-
-.v229_601 {
-  width: 62px;
-  color: rgba(170, 170, 170, 1);
-  position: absolute;
-  top: 14px;
-  left: 13px;
-  font-family: DungGeunMo;
-  font-weight: Regular;
-  font-size: 13px;
-  opacity: 1;
-  text-align: left;
-}
-
-.v229_631 {
+.footer {
   width: 217px;
   height: 41px;
-
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
   opacity: 1;
   position: absolute;
-  top: 660px;
-  left: 79px;
+  top: 521px;
+  left: 50px;
   overflow: hidden;
 }
 
@@ -375,172 +216,11 @@ body {
   overflow: hidden;
 }
 
-.v229_634 {
-  width: 33px;
-  color: rgba(255, 255, 255, 1);
-  position: absolute;
-  top: 12px;
-  left: 36px;
-  font-family: DungGeunMo;
-  font-weight: Regular;
-  font-size: 16px;
-  opacity: 1;
-  text-align: center;
-}
-
-.v229_635 {
-  width: 33px;
-  color: rgba(255, 255, 255, 1);
-  position: absolute;
-  top: 12px;
-  left: 149px;
-  font-family: DungGeunMo;
-  font-weight: Regular;
-  font-size: 16px;
-  opacity: 1;
-  text-align: center;
-}
-
 .name {
   color: #fff;
 }
 
-.v229_609 {
-  width: 375px;
-  height: 44px;
-  /*  background: url("../images/v229_609.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  padding: 11px 16px;
-  margin: 222px;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
 
-.v229_610 {
-  width: 54px;
-  color: rgba(20, 20, 20, 1);
-  position: absolute;
-  top: 12px;
-  left: 16px;
-  font-family: SF Pro Text;
-  font-weight: Semibold;
-  font-size: 15px;
-  opacity: 1;
-  text-align: center;
-}
-
-.v229_611 {
-  width: 67px;
-  height: 12px;
-  background: rgba(255, 255, 255, 1);
-  opacity: 1;
-  position: absolute;
-  top: 16px;
-  left: 292px;
-  overflow: hidden;
-}
-
-.v229_612 {
-  width: 24px;
-  height: 11px;
-  /*  background: url("../images/v229_612.png")*/;
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 42px;
-  overflow: hidden;
-}
-
-.v229_613 {
-  width: 22px;
-  height: 11px;
-  /*background: url("../images/v229_613.png");*/
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 0.3499999940395355;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  border: 1px solid rgba(20, 20, 20, 1);
-  border-top-left-radius: 2px;
-  border-top-right-radius: 2px;
-  border-bottom-left-radius: 2px;
-  border-bottom-right-radius: 2px;
-  overflow: hidden;
-}
-
-.v229_614 {
-  width: 1px;
-  height: 4px;
-  background: rgba(20, 20, 20, 1);
-  opacity: 0.4000000059604645;
-  position: absolute;
-  top: 3px;
-  left: 23px;
-}
-
-.v229_615 {
-  width: 18px;
-  height: 7px;
-  background: rgba(20, 20, 20, 1);
-  opacity: 1;
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  border-top-left-radius: 1px;
-  border-top-right-radius: 1px;
-  border-bottom-left-radius: 1px;
-  border-bottom-right-radius: 1px;
-  overflow: hidden;
-}
-
-.name {
-  color: #fff;
-}
-
-.name {
-  color: #fff;
-}
-
-/*.field {
-  margin: 20% auto;
-  width: 88%;
-  height: 80%;
-  background: url(../assets/images/field.png) no-repeat center transparent;
-  !*background-size: cover;*!
-}
-
-.title {
-  height: 20%;
-}
-.title p {
-  text-align: center;
-  margin: 0;
-  padding: 10%;
-}
-.content {
-  height: 60%;
-  display: flex;
-}
-
-.content .input-field {
-  margin: 10px auto;
-  width: 100%;
-  padding: 10px;
-}
-
-.input-field input {
-  width: 100%;
-}*/
 
 
 </style>
