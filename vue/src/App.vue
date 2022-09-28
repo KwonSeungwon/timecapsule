b4f2bc7dbba38d681493759b46bf96c0adccf38d
박성수 <pss@linkall.io>
2022-09-28 22:44:37 +0900

    input field 및 하단 힌트 텍스트 css

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -10,17 +10,32 @@ p,
 label {
   color: white;
   font-size: 14px;
+}
+
+.beach button, .beach p, .beach label {
+  text-shadow: 1px 1px #006484, -1px -1px #006484, 1px -1px #006484, -1px 1px #006484;
+}
+
+.mountain button, .mountain p, .mountain label {
   text-shadow: 1px 1px #315fd3, -1px -1px #315fd3, 1px -1px #315fd3, -1px 1px #315fd3;
 }
 
+.desert button, .desert p, .desert label {
+  text-shadow: 1px 1px #5E3432, -1px -1px #5E3432, 1px -1px #5E3432, -1px 1px #5E3432;
+}
+
+.space button, .space p, .space label {
+  text-shadow: 1px 1px #2D459F, -1px -1px #2D459F, 1px -1px #2D459F, -1px 1px #2D459F;
+}
+
 .title {
-  height: 15%;
+  height: 82px;
 }
 .content {
-  height: 70%;
+  height: calc(100% - 162px);
 }
 .footer {
-  height: 15%;
+  height: 70px;
   text-align: center;
 }
 .footer button.btn:not(:first-child) {
@@ -28,7 +43,7 @@ label {
 }
 .title p {
   margin: 0;
-  padding: 30px;
+  padding: 30px 20px;
   font-size: 22px;
 }
 
@@ -40,32 +55,100 @@ label {
   background-size: cover;
 }
 
-.input-field label {
-  display: inline-block;
+
+.content .input-field {
+  margin: 0 auto;
+  width: calc(100% - 40px);
+  padding: 20px;
+}
+
+.input-field>label {
+  display: block;
   width: 112px;
   height: 37px;
   text-align: center;
   line-height: 37px;
+  margin-bottom: 10px;
 }
 
-.beach .input-field label {
+.beach .input-field>label {
   background: url(./assets/images/beach/input_title.png) no-repeat center transparent;
   background-size: cover;
 }
 
-.desert .input-field label {
+.desert .input-field>label {
   background: url(./assets/images/desert/input_title.png) no-repeat center transparent;
   background-size: cover;
 }
 
-.mountain .input-field label {
+.mountain .input-field>label {
   width: 124px;
   background: url(./assets/images/mountain/input_title.png) no-repeat center transparent;
   background-size: cover;
 }
 
-.space .input-field label {
+.space .input-field>label {
   background: url(./assets/images/beach/input_title.png) no-repeat center transparent;
+  background-size: cover;
+}
+
+.input-field>input {
+  width: 273px;
+  height: 22px;
+  padding: 10px;
+  border: 0;
+  background: url(./assets/images/beach/input.png) no-repeat center transparent;
+  /*background-size: cover;*/
+}
+
+.input-field>p.hint {
+  font-size: 13px;
+  text-shadow: none;
+}
+
+.beach .input-field>p.hint,
+.beach .input-field>p.hint a {
+  color: #006484;
+}
+
+.mountain .input-field>p.hint,
+.mountain .input-field>p.hint a {
+  color: #264CAF;
+}
+
+.desert .input-field>p.hint,
+.desert .input-field>p.hint a{
+  color: #5E3432;
+}
+
+.space .input-field>p.hint,
+.space .input-field>p.hint a{
+  color: #2D459F;
+}
+
+.field {
+  margin: 20% auto;
+  width: 88%;
+  height: 80%;
+}
+
+.beach .field {
+  background: url(./assets/images/beach/bg.png) no-repeat center transparent;
+  background-size: cover;
+}
+
+.mountain .field {
+  background: url(./assets/images/mountain/bg.png) no-repeat center transparent;
+  background-size: cover;
+}
+
+.desert .field {
+  background: url(./assets/images/desert/bg.png) no-repeat center transparent;
+  background-size: cover;
+}
+
+.space .field {
+  background: url(./assets/images/mountain/bg.png) no-repeat center transparent;
   background-size: cover;
 }
 
