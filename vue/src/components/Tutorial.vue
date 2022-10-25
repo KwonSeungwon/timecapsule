cc7277ae28b342610af84ef03bf32d5affb85ecd
joranegs92 <joranegs92@gmail.com>
2022-10-25 22:20:01 +0900

    tutorial 이미지빼고 다 올림

--- vue/src/components/Tutorial.vue
+++ vue/src/components/Tutorial.vue
@@ -1,15 +1,61 @@
 <template>
   <div class="field-holder">
-    <swiper
-        :slides-per-view="3"
-        :space-between="50"
-        @swiper="onSwiper"
-        @slideChange="onSlideChange"
-    >
-      <swiper-slide role="tab"> s111
+    <swiper class="swiper" :slides-per-view="3" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+      <!--첫번째 슬라이드-->
+      <swiper-slide role="tab">
+        <div class="field">
+          <div class="title" style="padding: 4px;">
+            <p style="padding: 4px; font-size: 30px;">사용법</p>
+            <p style="padding: 4px; font-size: 15px;">타임캡슐 만들기 (MY)</p>
+          </div>
+          <div class="content" style="font-weight: normal;">
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">1.     좌표를 새로 생성하고<br>
+              비밀번호를 입력해주세요</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">2.     캡슐을 열어 편지지를 읽을 날짜를 <br>입력해주세요.</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">3.     원하는 타임캡슐을 선택해 주세요.</p>
+          </div>
+          <span class="paginationFirst">
+          </span>
+          <Footer style="margin-top:10px;" prev="닫기" one-button></Footer>
+        </div>
       </swiper-slide>
-      <swiper-slide role="tab">Slide 4</swiper-slide>
-      <swiper-slide role="tab">Slide 3</swiper-slide>
+      <!-- 두번째 슬라이드 -->
+      <swiper-slide role="tab">
+        <div class="field">
+          <div class="title" style="padding: 4px;">
+            <p style="padding: 4px; font-size: 30px;">사용법</p>
+            <p style="padding: 4px; font-size: 15px;">편지보내기 (Send)</p>
+          </div>
+          <div class="content" style="font-weight: normal;">
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">1.     받는사람의 타임캡슐 주소(url)와<br>
+              보내는 사람을 입력해주세요</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">2.     원하는 편지지를 선택해 주세요</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">3.     선택한 편지지에 내용을 써주세요</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">4.     완료를 클릭해 작성한 편지를 전송해주세요</p>
+          </div>
+          <span class="paginationMiddle">
+          </span>
+          <Footer style="margin-top:10px;" prev="닫기" one-button></Footer>
+        </div>
+      </swiper-slide>
+      <!-- 세번째 슬라이드 -->
+      <swiper-slide role="tab">
+        <div class="field">
+          <div class="title" style="padding: 4px;">
+            <p style="padding: 4px; font-size: 30px;">사용법</p>
+            <p style="padding: 4px; font-size: 15px;">타입캡슐 확인하기 (MY)</p>
+          </div>
+          <div class="content" style="font-weight: normal;">
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">1.     기존에 생성한 내 좌표와 비밀번호를 입력해주세요<br>
+              비밀번호를 입력해주세요</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">2.     타입캡슐로 들어온 편지지를 클릭해 내용을 <br>확인해주세요.</p>
+            <p style="color: #522a2a; text-shadow: none; margin-top: 30px;">3.     편지는 한 화면에 최대 6개까지 노출되며, 화면을 좌우로 스와이프하여 이전/다음 6개의 편지를 확인할수 있어요.</p>
+          </div>
+          <span class="paginationLast">
+          </span>
+          <Footer style="margin-top:10px;" prev="닫기" one-button></Footer>
+        </div>
+      </swiper-slide>
       <swiper-slide role="slider">Slide 4</swiper-slide>
       <swiper-slide role="slider">Slide 5</swiper-slide>
       <swiper-slide role="slider">Slide 6</swiper-slide>
@@ -23,7 +69,7 @@
 </template>
 
 <script>
-/*import Footer from '@/components/Footer';*/
+import Footer from '@/components/Footer';
 
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -33,7 +79,7 @@ import 'swiper/css';
 export default {
   name: "cTutorial",
   components : {
-  /*  Footer,*/
+    Footer,
     Swiper,
     SwiperSlide,
   },
@@ -43,7 +89,7 @@ export default {
       tutorialItem : [1,2,3],
       selectIdx : 0,
       slidesPerView: 'auto',
-      spaceBetween: 6, // swiper-slide 사이의 간격 지정
+      spaceBetween: 20, // swiper-slide 사이의 간격 지정
       slidesOffsetBefore: 0, // slidesOffsetBefore는 첫번째 슬라이드의 시작점에 대한 변경할 때 사용
       slidesOffsetAfter: 0, // slidesOffsetAfter는 마지막 슬라이드 시작점 + 마지막 슬라이드 너비에 해당하는 위치의 변경이 필요할 때 사용
       freeMode: true, // freeMode를 사용시 스크롤하는 느낌으로 구현 가능
@@ -82,7 +128,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.swiper-container {
+/*.swiper-container {
   .swiper-wrapper {
     .swiper-slide {
       width: auto; // auto 값을 지정해야 슬라이드의 width값이 텍스트 길이 기준으로 바뀜
@@ -99,7 +145,23 @@ export default {
       cursor: pointer;
     }
   }
+}*/
+.swiper {
+
+  height: 830px;
+  width: 100%;
+
+  .swiper-slide {
+    width: auto;
+    min-width: 297.6667px;
+    margin-right: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+  }
 }
+
 </style>
 <style scoped>
 
@@ -117,10 +179,11 @@ export default {
 .field {
   position: absolute;
   left: 17px;
+  top: 10px;
   margin: 20% auto;
   padding: 25px 20px;
   width: 301px;
-  height: 602px;
+  height: 68%;
   transition: all .5s;
 }
 
@@ -142,28 +205,41 @@ export default {
   width: 1px;
 }
 
-.pagination {
+/*.pagination {
   width: 43px;
   height: 9px;
   display: block;
-}
+}*/
 
-.pagination.first {
+.paginationFirst {
+  margin-left: 43%;
+  width: 43px;
+  height: 9px;
+  display: block;
   background: url(../assets/images/pagination-first.png) no-repeat center transparent;
   background-size: cover;
 }
 
-.pagination.middle {
+.paginationMiddle {
+  margin-left: 43%;
+  width: 43px;
+  height: 9px;
+  display: block;
   background: url(../assets/images/pagination-middle.png) no-repeat center transparent;
   background-size: cover;
 }
 
-.pagination.last {
+.paginationLast {
+  margin-left: 43%;
+  width: 43px;
+  height: 9px;
+  display: block;
   background: url(../assets/images/pagination-last.png) no-repeat center transparent;
   background-size: cover;
 }
 
 .beach .field {
+
   background: url(../assets/images/field_bc.png) no-repeat center transparent;
   background-size: cover;
 }
