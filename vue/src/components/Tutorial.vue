49d5626b2b7268203cb992845785d9da37f401af
joranegs92 <joranegs92@gmail.com>
2022-10-25 20:55:12 +0900

    tutorial 수정단계

--- vue/src/components/Tutorial.vue
+++ vue/src/components/Tutorial.vue
@@ -1,37 +1,53 @@
 <template>
   <div class="field-holder">
+    <swiper
+        :slides-per-view="3"
+        :space-between="50"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+    >
+      <swiper-slide role="tab"> s111
+      </swiper-slide>
+      <swiper-slide role="tab">Slide 4</swiper-slide>
+      <swiper-slide role="tab">Slide 3</swiper-slide>
+      <swiper-slide role="slider">Slide 4</swiper-slide>
+      <swiper-slide role="slider">Slide 5</swiper-slide>
+      <swiper-slide role="slider">Slide 6</swiper-slide>
+      <swiper-slide role="slider">Slide 7</swiper-slide>
+      ...
+    </swiper>
     <transition-group :name="transition">
-      <div class="field"
-           v-for="(item, idx) in tutorialItem" :key="idx"
-           :class="{'prev' : selectIdx > idx, 'next' : selectIdx < idx, 'none' : selectIdx - idx > 1 || selectIdx - idx < -1}"
-           @click="moveField(selectIdx - 1 === idx ? 'prev' : selectIdx + 1 === idx ? 'next' : false)">
-        <div class="title">
-          <p>사용법</p>
-        </div>
-        <div class="content">
-          <p>1.{{item}}</p>
-          <p>2.</p>
-          <p>3.</p>
-          <p>4.</p>
-        </div>
-        <span class="pagination" :class="{'first' : idx === 0, 'middle' : idx > 0 && tutorialItem.length > idx, 'last' : idx === tutorialItem.length - 1 && idx !== 0}"></span>
-        <Footer prev="닫기" one-button></Footer>
-      </div>
+
     </transition-group>
   </div>
 </template>
 
 <script>
-import Footer from '@/components/Footer';
+/*import Footer from '@/components/Footer';*/
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// Import Swiper styles
+import 'swiper/css';
 
 export default {
   name: "cTutorial",
-  components : {Footer},
+  components : {
+  /*  Footer,*/
+    Swiper,
+    SwiperSlide,
+  },
   data () {
     return {
       transition : 'slide-next', //slide-next, slide-prev
       tutorialItem : [1,2,3],
-      selectIdx : 0
+      selectIdx : 0,
+      slidesPerView: 'auto',
+      spaceBetween: 6, // swiper-slide 사이의 간격 지정
+      slidesOffsetBefore: 0, // slidesOffsetBefore는 첫번째 슬라이드의 시작점에 대한 변경할 때 사용
+      slidesOffsetAfter: 0, // slidesOffsetAfter는 마지막 슬라이드 시작점 + 마지막 슬라이드 너비에 해당하는 위치의 변경이 필요할 때 사용
+      freeMode: true, // freeMode를 사용시 스크롤하는 느낌으로 구현 가능
+      centerInsufficientSlides: true, // 컨텐츠의 수량에 따라 중앙정렬 여부를 결정함
     }
   },
   methods : {
@@ -50,10 +66,41 @@ export default {
     nextIdx() {
       if (this.tutorialItem.length > this.selectIdx + 1)  this.transition = 'slide-next'; this.selectIdx++;
     }
-  }
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
 }
 </script>
-
+<style lang="scss" scoped>
+.swiper-container {
+  .swiper-wrapper {
+    .swiper-slide {
+      width: auto; // auto 값을 지정해야 슬라이드의 width값이 텍스트 길이 기준으로 바뀜
+      min-width: 56px; // min-width를 지정하지 않을 경우 텍스트가 1개 내지는 2개가 들어갈 때 탭 모양이 상이할 수 있으므로 넣어준다.
+      padding: 0px 14px;
+      font-size: 14px;
+      line-height: 36px;
+      text-align: center;
+      color: #84868c;
+      border: 0;
+      border-radius: 18px;
+      background: #f3f4f7;
+      appearance: none;
+      cursor: pointer;
+    }
+  }
+}
+</style>
 <style scoped>
 
 /*.title p {*/
