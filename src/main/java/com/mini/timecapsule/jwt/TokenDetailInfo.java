1e379d73338dc9f3a5a8c2ad09271e74b1df4477
hyeonsu <joranegs92@gmail.com>
2023-01-11 17:53:33 +0900

    jwt

---
+++ src/main/java/com/mini/timecapsule/jwt/TokenDetailInfo.java
@@ -0,0 +1,46 @@
+package com.mini.timecapsule.jwt;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 토큰 상세 정보
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TokenDetailInfo implements Serializable {
+
+    private static final long serialVersionUID = 2730027757782228759L;
+
+    /**
+     * 토큰 정보
+     */
+    private String token;
+    /**
+     * 토큰 상태 정보
+     */
+    private TokenStatus tokenStatus;
+    /**
+     * 토큰 발급 정보
+     */
+    private TokenType tokenType;
+    /**
+     * tokenType
+     *  WEB-userId
+     *  APP-loginId
+     */
+    private String id;
+    /**
+     * tokenType
+     *  WEB-userNm
+     *  APP-patientNm
+     */
+    private String name;
+}
